ad60fb8270d7432224efadeda4601798bd8fbc5a
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2023-12-14 18:50:15 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,40 @@
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+def generate_random_data():
+
+    main()
+def main():
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
+if __name__ == "__main__":
+
+    data = generate_random_data()
+    main()
+
+if __name__ == "__main__":
+def generate_random_data():
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+def main():
+    return data    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+import random
+
+    main()
+
+
+    for item in data:
+def generate_random_data():
+    return data
+
+    data = generate_random_data()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
